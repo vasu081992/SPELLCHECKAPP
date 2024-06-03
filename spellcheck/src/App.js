34739638b5d28de6c1b089d5763fb656276c03e9
vasuve_altair <vasuve@altair.com>
2024-06-03 12:38:27 +0530

    spell check final commit

---
+++ spellcheck/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import SpellCheckApp from './components/Spellcheck';
+function App() {
+  return (
+    <div className="App">
+<SpellCheckApp/>
+    </div>
+  );
+}
+
+export default App;
